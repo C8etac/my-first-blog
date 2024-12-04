c11b7f0c68f40cdbfe66a03666202cfc243986c7
C8etac <Catelynn.Barfell@icloud.com>
2024-12-04 11:06:30 -0500

    Added homework.

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -123,3 +122,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/'
